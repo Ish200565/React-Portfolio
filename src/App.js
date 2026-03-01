669d74e4e48b3ccf57bd2a28ddc2811d8ffba52e
Ish200565 <ishikabelel@gmail.com>
2026-03-01 18:44:39 +0530

    Add portfolio components

---
+++ src/App.js
@@ -0,0 +1,15 @@
+/*The main assembler*/
+import './App.css'; 
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Portfolio</h1>
+        <p>This is a React Portfolio project.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
